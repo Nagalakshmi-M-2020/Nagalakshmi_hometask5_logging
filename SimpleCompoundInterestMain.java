8e52a89555fac88f7b119f964fbfbb94b3e4c802
Nagalakshmi-M-2020 <60350622+Nagalakshmi-M-2020@users.noreply.github.com>
2020-02-25 18:30:39 +0530

    Add files via upload

---
+++ SimpleCompoundInterestMain.java
@@ -0,0 +1,9 @@
+package hometask5;
+import hometask5.SimpleCompoundInterest;
+
+public class SimpleCompoundInterestMain extends SimpleCompoundInterest{
+	public static void main(String args[]) {
+	    SimpleCompoundInterest logic = new SimpleCompoundInterest();
+	    logic.calculate();
+	}
+}
